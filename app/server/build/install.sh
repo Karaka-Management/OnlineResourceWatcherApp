307198d90d0c00bbd069dcff10e45d80a1897829
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-11-04 22:54:36 +0100

    add dependency

--- app/server/build/install.sh
+++ app/server/build/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sudo apt-get install libcurl4-openssl-dev
-sudo apt-get install sqlite3 libsqlite3-dev
-sudo apt install default-libmysqlclient-dev
-sudo apt-get install libxml2-dev
+sudo apt-get install libcurl4-openssl-dev # curl
+sudo apt-get install sqlite3 libsqlite3-dev # sqlite
+sudo apt install default-libmysqlclient-dev # mysql
+sudo apt-get install libxml2-dev # xml
+sudo apt-get install libpq-dev # postgresql
 
 # Windows
 # regex http://gnuwin32.sourceforge.net/packages/pcre.htm
