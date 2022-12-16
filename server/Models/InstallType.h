045144c7fe9c4c9e6d065d3112e955b6d852c8a1
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-12-16 21:59:21 +0100

    fix url

--- server/Models/InstallType.h
+++ server/Models/InstallType.h
@@ -5,7 +5,7 @@
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
- * @link      https://karaka.app
+ * @link      https://jingga.app
  */
 #ifndef MODELS_INSTALL_TYPE_H
 #define MODELS_INSTALL_TYPE_H
