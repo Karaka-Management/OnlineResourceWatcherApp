b3050d6b2f58b31f14ebfc7b6cd7152d778c66bd
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:15 +0200

    fix version and bugs

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\OnlineResourceWatcher
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -20,7 +20,7 @@ use phpOMS\Module\ModuleAbstract;
  * OnlineResourceWatcher controller class.
  *
  * @package Modules\OnlineResourceWatcher
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
